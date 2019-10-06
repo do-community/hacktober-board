900091c31016492ee9809160255b047ce9ff25b3
Burak Ozdemir <burakozdemir@protonmail.ch>
2019-10-07 01:06:11 +0300

    fix seed generations scripts, fixes issue #1

--- database/factories/IssuesFactory.php
+++ database/factories/IssuesFactory.php
@@ -5,11 +5,16 @@ use App\Issue;
 use Faker\Generator as Faker;
 
 $factory->define(Issue::class, function (Faker $faker) {
+    $project = App\Project::inRandomOrder()->first();
     return [
         'id' => $faker->unique()->randomNumber(6),
         'number' => $faker->randomNumber(3),
         'title' => $faker->sentence(6, true),
         'body' => $faker->paragraphs(3, true),
+        'html_url' => $faker->url,
+        'project_id' => $project->id,
+        'project_language' => $project->language,
+        'user_id'    => App\User::inRandomOrder()->first()->id,
         'original_created_at' => $faker->dateTimeThisMonth(),
         'original_updated_at' => $faker->dateTimeThisMonth()
     ];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LabelsTableSeeder::class);
+        $this->call(ProjectsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
     }
 }
